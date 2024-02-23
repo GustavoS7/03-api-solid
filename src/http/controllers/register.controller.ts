ed8eba0a798f99285ae5f2aa418ec8b2c79557f4
GustavoS7 <gustavods2003@outlook.com>
2024-02-22 23:20:25 -0300

    handling error

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -1,4 +1,5 @@
 import { PrismaUserRepository } from "@/repositories/prisma/prisma-users-repository";
+import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
 import { RegisterUseCase } from "@/use-cases/register";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
@@ -21,7 +22,10 @@ export async function register(
 
     await registerUseCase.execute({ name, email, password });
   } catch (error) {
-    return reply.status(409).send();
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message });
+    }
+    return reply.status(500).send();
   }
 
   return reply.status(201).send();
