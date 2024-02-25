b6388b70f47ae98747924a63e107ef4e21dfe498
GustavoS7 <gustavods2003@outlook.com>
2024-02-25 20:41:50 -0300

    implementing check-ins repository prisma

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,6 +3,16 @@ import { Prisma, User } from "@prisma/client";
 import { prisma } from "@/lib/prisma";
 
 export class PrismaUserRepository implements UsersRepository {
+  async findById(id: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  }
+
   async findByEmail(email: string): Promise<User | null> {
     const user = await prisma.user.findUnique({
       where: {
