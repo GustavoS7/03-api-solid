c7afc427a670b3e877292d3ff1c84c2f4da92066
GustavoS7 <gustavods2003@outlook.com>
2024-02-25 19:35:17 -0300

    count check-ins vy uder use case

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -18,6 +18,10 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return checkIn;
   }
 
+  async countByUserId(userId: string): Promise<number> {
+    return this.items.filter((item) => item.user_id === userId).length;
+  }
+
   async findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
     return this.items
       .filter((item) => item.user_id === userId)
