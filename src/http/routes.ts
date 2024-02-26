b3adb3877dc332d2ea952e855b5d7e039c5138da
GustavoS7 <gustavods2003@outlook.com>
2024-02-25 22:57:43 -0300

    jwt authorization

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import { authenticate } from "./controllers/authenticate.controller";
 import { register } from "./controllers/register.controller";
+import { verifyJWT } from "./middlewares/verify-jwt";
+import { profile } from "./controllers/profile";
 import { FastifyInstance } from "fastify";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
+
+  app.get("/me", { onRequest: [verifyJWT] }, profile);
 }
