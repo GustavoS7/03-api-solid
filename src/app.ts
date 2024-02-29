229219b9b301aa799331159137d2cd65c55f0b48
GustavoS7 <gustavods2003@outlook.com>
2024-02-28 22:51:02 -0300

    refreshing token

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { checkInRoutes } from "./http/controllers/check-ins/routes";
 import { usersRoutes } from "./http/controllers/users/routes";
 import { gymsRoutes } from "./http/controllers/gyms/routes";
+import fastifyCookie from "@fastify/cookie";
 import fastifyJwt from "@fastify/jwt";
 import { ZodError } from "zod";
 import fastify from "fastify";
@@ -10,7 +11,16 @@ export const app = fastify();
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
 });
+
+app.register(fastifyCookie);
 
 app.register(usersRoutes);
 app.register(gymsRoutes);
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,12 +1,15 @@
 import { verifyJWT } from "../../middlewares/verify-jwt";
 import { authenticate } from "./authenticate.controller";
 import { register } from "./register.controller";
+import { refresh } from "./refresh.controller";
 import { FastifyInstance } from "fastify";
 import { profile } from "./profile";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
+
+  app.patch("/token/refresh", refresh);
 
   app.get("/me", { onRequest: [verifyJWT] }, profile);
 }
